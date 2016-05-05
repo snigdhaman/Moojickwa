f36f5740578258d49494b8deac1db43c44fee5d0
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:33:43 +0530

    Moojickwa
    A simple music player app

--- Moojickwa/ViewController.swift
+++ Moojickwa/ViewController.swift
@@ -7,12 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    @IBOutlet weak var scrubOutlet: UISlider!
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        player.play()
+        
+    }
+    
+    @IBAction func scrubAction(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubOutlet.value)
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    @IBAction func sliderAction(sender: AnyObject) {
+        
+        player.volume = sliderOutlet.value
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        initialise()
+        
+    }
+    
+    func initialise() {
+    
+        if let filePath = NSBundle.mainBundle().pathForResource("bach-bwv651-breemer", ofType: "mp3") {
+            
+            do {
+                
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: filePath))
+                
+                player.volume = sliderOutlet.value
+                scrubOutlet.maximumValue = Float(player.duration)
+                scrubOutlet.value = 0
+                
+            } catch {
+                
+                print("Error")
+                
+            }
+            
+        }
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initialise()
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updateSlider"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func updateSlider () {
+        
+        scrubOutlet.value = Float(player.currentTime)
+        
     }
 
     override func didReceiveMemoryWarning() {
